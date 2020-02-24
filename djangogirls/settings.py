a6ae68028037f90f974edc41bc0664d996b598aa
patrick charlson <patrickcharlson@gmail.com>
2020-02-24 19:37:04 +0300

    Changed the HTML for the site

--- djangogirls/settings.py
+++ djangogirls/settings.py
@@ -53,7 +53,7 @@ ROOT_URLCONF = 'djangogirls.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, 'blog/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
